48a12276c45a8ab31bc3afafdcb86c7a596da7ad
gu1march <98661325+gu1march@users.noreply.github.com>
2022-05-16 11:22:56 -0300

    Update AreaCalculavel.java

--- ExercicioGeometria/src/AreaCalculavel.java
+++ ExercicioGeometria/src/AreaCalculavel.java
@@ -2,7 +2,7 @@
 public interface AreaCalculavel {
 	
 public default void calcularArea() {
-	
+//Interface só recebe métodos e não atributos, além de ser implementada usando (implements), é possível implementar mais de uma interface diferente da super classe//
 }
 	
 	
