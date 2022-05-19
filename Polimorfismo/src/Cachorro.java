f9ec8590c1050b4e0a9368f360c0ca7bd0ec3f39
gu1march <98661325+gu1march@users.noreply.github.com>
2022-05-19 07:39:38 -0300

    Update Cachorro.java

--- Polimorfismo/src/Cachorro.java
+++ Polimorfismo/src/Cachorro.java
@@ -1,6 +1,7 @@
 
 public class Cachorro extends Animal {
 
+	//Override serve para sobreescrever um método de outra classe(superclasse)//
 	@Override
 	public void falar() {
 		System.out.println("AU AU");
