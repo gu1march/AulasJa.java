83fe5b5710bf40f0e602bd0c6762569727426364
gu1march <98661325+gu1march@users.noreply.github.com>
2022-05-23 07:58:10 -0300

    Update LeiteQuente.java

--- PolimorfismoBebidas/src/LeiteQuente.java
+++ PolimorfismoBebidas/src/LeiteQuente.java
@@ -2,12 +2,12 @@
 public class LeiteQuente extends Bebida{
 
 	public LeiteQuente() {
-		super("Leite quente", true);
+		super("Leite quente", true); //o super indica que perence a super-classe ou classe pai//
 	}
 
 	@Override
 	public void preparar() {
-		super.preparar();
+		super.preparar();  //o super indica que perence a super-classe ou classe pai//
 	}
 	
 }
