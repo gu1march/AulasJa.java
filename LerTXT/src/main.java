6e7208e59e79252a9b666fabc94548aa0844a364
gu1march <98661325+gu1march@users.noreply.github.com>
2022-05-26 10:15:10 -0300

    Add files via upload

---
+++ LerTXT/src/main.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class main {
+
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		
+		try(BufferedReader reader = new BufferedReader(new FileReader("entrada.txt"))){
+			String line;
+			String string = "";
+			
+			while ((line = reader.readLine())!= null) {
+				string += line + "\n";
+			}
+			System.out.println(string);
+			
+			
+		}
+		
+		
+		
+		
+	}
+
+}
